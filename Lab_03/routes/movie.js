b087a6c00dda50fe5a10a75492dcbb02719a46c1
Yuqi Wang <wangyq961129@outlook.com>
2020-10-04 19:37:12 -0400

    Lab_03 code finished, lack css

--- Lab_03/routes/movie.js
+++ Lab_03/routes/movie.js
@@ -6,6 +6,7 @@ const redis = require('redis');
 const client = redis.createClient();
 
 const data = require('../data/data');
+const e = require('express');
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
@@ -14,7 +15,7 @@ client.on('error', (error) => console.log(error));
 client.on('connected', () => console.log('Connected to Redis...'));
 
 router.get('/', async (req, res) => {
-    let homePage = await client.getAsync('homePage');
+    const homePage = await client.getAsync('homePage');
     if (!homePage) {
         const allData = await data.getList();
         res.render(
@@ -22,6 +23,7 @@ router.get('/', async (req, res) => {
             {
                 layout: 'main',
                 title: 'Home Page',
+                token: false,
                 allData: allData,
             },
             async (err, html) => {
@@ -29,31 +31,90 @@ router.get('/', async (req, res) => {
                 res.send(html);
             }
         );
-        return;
+    } else res.send(homePage);
+});
+
+router.get('/popularsearches', async (req, res) => {
+    try {
+        const top10 = await client.zrevrangeAsync('popSearch', 0, 9);
+        res.render('view/popular', {
+            layout: 'main',
+            title: 'Top 10 Searches',
+            top10: top10,
+        });
+    } catch (e) {
+        res.render('view/error', { layout: 'main' });
     }
-    res.send(homePage);
 });
 
 router.get('/:id', async (req, res) => {
     const id = req.params.id;
-    let detailPage = await client.getAsync(id);
+    const detailPage = await client.getAsync(id);
     if (!detailPage) {
-        const detail = await data.getId(id);
-        res.render(
-            'view/detail',
-            {
-                layout: 'main',
-                title: `${detail.name}`,
-                detail: detail,
-            },
-            async (err, html) => {
-                await client.setAsync(id, html.toString());
-                res.send(html);
+        try {
+            const detail = await data.getId(id);
+            if (!detail) {
+                res.render('view/error', { layout: 'main' });
+            } else {
+                res.render(
+                    'view/detail',
+                    {
+                        layout: 'main',
+                        title: `${detail.name}`,
+                        detail: detail,
+                    },
+                    async (err, html) => {
+                        await client.setAsync(id, html.toString());
+                        res.send(html);
+                    }
+                );
             }
-        );
-        return;
-    }
-    res.send(detailPage);
+        } catch (e) {
+            res.render('view/error', { layout: 'main' });
+        }
+    } else res.send(detailPage);
 });
+
+router.post('/search', async (req, res) => {
+    const query = req.body.query;
+    if (!queryCheck(query)) {
+        res.render('view/error', { layout: 'main' });
+    } else {
+        const searchPage = await client.getAsync(query);
+        if (!searchPage) {
+            try {
+                let allData = await data.getQuery(query);
+                for (let i = 0; i < allData.length; i++) {
+                    allData[i] = allData[i].show;
+                }
+                res.render(
+                    'view/home',
+                    {
+                        layout: 'main',
+                        title: `Result for '${query}'`,
+                        token: true,
+                        allData: allData,
+                    },
+                    async (err, html) => {
+                        await client.setAsync(query, html.toString());
+                        await client.zaddAsync('popSearch', 1, query);
+                        res.send(html);
+                    }
+                );
+            } catch (e) {
+                res.render('view/error', { layout: 'main' });
+            }
+        } else {
+            const val = await client.zscoreAsync('popSearch', query);
+            await client.zaddAsync('popSearch', parseInt(val) + 1, query);
+            res.send(searchPage);
+        }
+    }
+});
+
+const queryCheck = (query) => {
+    const check = /[a-z]/i;
+    return check.test(query);
+};
 
 module.exports = router;
